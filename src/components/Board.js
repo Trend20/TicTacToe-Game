186a4162e28e9052a47d172cb97bceffa65490aa
Trend20 <enockomondi305@gmail.com>
2020-05-05 08:23:37 -0400

    board

--- src/components/Board.js
+++ src/components/Board.js
@@ -7,27 +7,7 @@ class Board extends React.Component {
         squares: Array(9).fill(null),
         XisNext: true,
       }
-      const calculateWinner = this.calculateWinner.bind('this');
     }
-    calculateWinner(squares) {
-        const lines = [
-          [0, 1, 2],
-          [3, 4, 5],
-          [6, 7, 8],
-          [0, 3, 6],
-          [1, 4, 7],
-          [2, 5, 8],
-          [0, 4, 8],
-          [2, 4, 6],
-        ];
-        for (let i = 0; i < lines.length; i++) {
-          const [a, b, c] = lines[i];
-          if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-            return squares[a];
-          }
-        }
-        return null;
-      }
     handleClick(i){
       const squares = this.state.squares.slice();
       if(calculateWinner(squares) || squares[i]){
@@ -42,6 +22,25 @@ class Board extends React.Component {
     renderSquare(i) {
       return <Square value={this.state.squares[i]}
                onClick={() =>this.handleClick(i)}/>;
+    }
+    calculateWinner = (squares) => {
+      const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+      ];
+      for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+          return squares[a];
+        }
+      }
+      return null;
     }
   
     render() {
